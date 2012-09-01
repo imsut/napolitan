6bb8837cd212efe7c3063691aad9ee99d31cc5cd
Ken Kawamoto <kentaro.kawamoto@gmail.com>
2012-09-02 01:56:21 +0900

    code cleanup

--- src/Handler/Stats.hs
+++ src/Handler/Stats.hs
@@ -3,12 +3,11 @@ module Handler.Stats where
 
 import Import
 
-import Data.Aeson (toJSON)
+import Data.Maybe
 import Data.Text (unpack)
 import Data.Time.Calendar (Day)
 import qualified Data.Time.Format as F
 import qualified Data.Time.LocalTime as L
-import DebugUtil
 import System.Locale (defaultTimeLocale)
 
 getStatsR :: Text -> Handler RepHtml
@@ -17,13 +16,11 @@ getStatsR _ = do
 
 getDailyStatsR :: Text -> Text -> Handler RepJson
 getDailyStatsR screenName dateString = do
-    day <- liftIO $ case mday of
-      Nothing -> today
-      Just d -> return d
+    -- works, but ugly
+    day <- liftIO $ fmap (flip fromMaybe mday) today
     tasks <- runDB $ do
       Just (Entity uid _) <- getBy $ UniqueName screenName
       selectList [ FinishedTaskUserId ==. uid, FinishedTaskFinishOn ==. day ] []
-    liftIO $ debugLog $ show $ toJSON $ fmap entityVal tasks
     jsonToRepJson $ fmap entityVal tasks
   where
     mday = F.parseTime defaultTimeLocale "%Y-%m-%d" $ unpack dateString :: Maybe Day
